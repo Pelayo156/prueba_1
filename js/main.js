3388f6da2fe9544487b1dbceb8e43fc8bae98a5b
bvaldenegro <bra.valdenegro@duocuc.cl>
2024-05-18 12:05:31 -0400

    Modo oscuro en index
    Se añaden las imagenes que son png (LO JURO!) la mayoría transparentes. También se crea el modo oscuro para la vista index de momento.

--- js/main.js
+++ js/main.js
@@ -1,57 +1,57 @@
 $(document).ready(function() {
-    // Creo una "base de datos" local
+    // Creación de una "base de datos" local
     const productos = [
         {
             id: 1,
             nombre: "Bravery Salmon Adult (4kg) Alimento para perro",
             precio: 26990,
-            url_image: "./images/bravery_salmon_adult_perro.jpeg"
+            url_image: "./images/bravery_salmon_adult_perro.png"
         },
         {
             id: 2,
             nombre: "Bravery Chicken Adult (2kg) Alimento para gato",
             precio: 21990,
-            url_image: "./images/bravery_chicken_adult_gato.jpeg"
+            url_image: "./images/bravery_chicken_adult_gato.png"
         },
         {
             id: 3,
             nombre: "Fit Formula arena para gato 3.2kg",
             precio: 14990,
-            url_image: "./images/fit_formula_arena_gato.jpg"
+            url_image: "./images/fit_formula_arena_gato.png"
         },
         {
             id: 4,
             nombre: "Fit Formula Adulto (10kg) Alimento para gato",
             precio: 24792,
-            url_image: "./images/fit_formula_adult_gato.jpeg"
+            url_image: "./images/fit_formula_adult_gato.png"
         },
         {
             id: 5,
             nombre: "Fit Formula Adulto (20kg) Alimento para perro",
             precio: 37592,
-            url_image: "./images/fit_formula_adult_perro.jpeg"
+            url_image: "./images/fit_formula_adult_perro.png"
         },
         {
             id: 6,
             nombre: "Bravery Salmon Adult (2kg) Alimento para gato",
             precio: 22990,
-            url_image: "./images/bravery_salmon_adult_gato.jpeg"
+            url_image: "./images/bravery_salmon_adult_gato.png"
         },
         {
             id: 7,
             nombre: "Bravery Pork Adult (2kg) Alimento para perro",
             precio: 28990,
-            url_image: "./images/bravery_pork_adult_perro.jpeg"
+            url_image: "./images/bravery_pork_adult_perro.png"
         },
         {
             id: 8,
             nombre: "Premier gatos adultos de pelo largo (7.5kg)",
             precio: 46990,
-            url_image: "./images/premier_gatos_pelo_largo.jpeg"
+            url_image: "./images/premier_gatos_pelo_largo.png"
         }
     ];
 
-    // Creo una lista para guardar los productos que van a estar en el carrito de compra.
+    // Creación de una lista para guardar los productos que van a estar en el carrito de compra.
     let productos_carrito = [];
 
     if (localStorage.getItem("productos_carrito")) {
@@ -63,7 +63,7 @@ $(document).ready(function() {
         let templates = [];
 
         lista_productos.forEach(producto => {
-            // Creo una plantilla para agregar el html al carrito de compras.
+            // Creación de una plantilla para agregar el html al carrito de compras.
             templates.push(
                 `
                 <div class="container producto-carrito">
@@ -101,7 +101,7 @@ $(document).ready(function() {
             }
         });
 
-        // Almaceno los productos escogidos al localstorage.
+        // Almacenamiento de los productos escogidos al localstorage.
        if(localStorage.getItem("productos_carrito")) {
             localStorage.removeItem("productos_carrito");
             
@@ -140,4 +140,14 @@ $(document).ready(function() {
             }
         }
     });
+
+    const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
+            function cambiaTema(ev){
+                if(ev.target.checked){
+                    document.documentElement.setAttribute('tema', 'light');
+                } else {
+                    document.documentElement.setAttribute('tema', 'dark');
+                }
+            }
+            colorSwitch.addEventListener('change', cambiaTema);
 });
